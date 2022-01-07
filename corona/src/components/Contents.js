9203c01f14ed1c40917e642e654c14983beb1610
iokl1004 <iokl3319@naver.com>
2022-01-07 18:06:08 +0900

    code Scalper

---
+++ corona/src/components/Contents.js
@@ -0,0 +1,29 @@
+import {useState, useEffect} from 'react'
+import { Bar, Doughnut, Line } from "react-chartjs2"
+import axios from 'axios'
+const Contents = () => {
+
+    useEffect(() => {
+        
+        const fetchEvents = async() =>{
+            const res = await axios.get("https://api.covid19api.com/total/dayone/country/kr");
+            console.log(res)
+        }
+
+        fetchEvents()
+    })
+    return (
+        <section>
+            <h2>국내 코로나 현황</h2>
+            <div className="contents">
+                <div>
+                    <Bar data={confirmeData} options={
+                        {title : {display:true, }}
+                    }
+                </div>  
+            </div>
+        </section>
+    )
+}
+
+export default Contents
